789203c0affedf916a0639df4c2e2dd5e3791a9b
Denis Alayza <denis.alayza@gmail.com>
2020-04-21 13:58:28 -0300

    Backend Todo app v1

---
+++ todo-app/backend/src/config/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+module.exports = (server) => {
+    // API routes
+    const router = express.Router()
+    server.use('/api', router)
+
+    // TODO Routes
+    const todoService = require('../api/todo/todoService')
+    todoService.register(router, '/todos')
+}
